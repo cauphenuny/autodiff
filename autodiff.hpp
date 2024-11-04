09d7b9f3fe413c6e8f9008695979c1acd694dcb0
cauphenuny <starseekist@gmail.com>
2024-11-04 14:33:04 +0800

    fix build

--- autodiff.hpp
+++ autodiff.hpp
@@ -12,13 +12,9 @@
 #    define debug(x)    std::cerr << #x << " = " << (x) << " "
 #    define debugln(x)  std::cerr << #x << " = " << (x) << std::endl
 #    define debugf(...) fprintf(stderr, __VA_ARGS__)
-#    if defined(__clang__) || defined(__GNUC__)
-#        define debugv(fmt, ...)                                    \
-            fprintf(stderr, "%s/%d: " fmt "\n", __func__, __LINE__, \
-                    ##__VA_ARGS__)
-#    else
-#        define debugv(...)
-#    endif
+#    define debugv(fmt, ...)                                    \
+        fprintf(stderr, "%s/%d: " fmt "\n", __func__, __LINE__, \
+                __VA_OPT__(, ) __VA_ARGS__)
 #else
 #    define debug(x)
 #    define debugln(x)
@@ -138,7 +134,7 @@ public:
         while (q.size()) {
             TapeNode* cur = q.front();
             // debugln(cur->to_string());
-            q.pfunc();
+            q.pop();
             // if (!cur->require_diff) continue;
             TapeNode *l = cur->left, *r = cur->right;
             switch (cur->func) {
