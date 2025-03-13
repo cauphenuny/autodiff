4c9969d58007461cf53c3d4f9b32ed10cee3f53e
cauphenuny <starseekist@gmail.com>
2025-03-13 15:36:51 +0800

    feat: auto unittests; several optimize algorithm

--- demo.cpp
+++ demo.cpp
@@ -1,4 +1,4 @@
-#include "autodiff.hpp"
+#include "src/variable.hpp"
 
 #include <cassert>
 #include <cmath>
@@ -11,34 +11,27 @@ template <typename T> T f(T x, T y, T z) { return log(x * z) + x * y - sin(y) + 
 
 template <typename T> T g(T x, T y, T z) { return z * pow(x, y); }
 
-auto numdiff(auto f, auto eps, auto... args) {
+auto num_diff(auto f, auto eps, auto... args) {
     return (f((args + eps)...) - f(args...)) / eps;
 }
 
 int main() {
     double eps = 1e-7, eqeps = 1e-4;
     double x0 = 2, y0 = 5, z0 = 3;
-    using autodiff::var;
 
     var x = x0, y = y0, z = z0;
     var u = f(x, y, z);
     auto [ux, uy, uz] = u.derivative(x, y, z);
     cout << format("u = {:.5}, ux = {:.5}, uy = {:.5}, uz = {:.5}\n", u, ux, uy, uz);
-    assert(abs((ux + uy + uz) - numdiff(f<double>, eps, x0, y0, z0)) < eqeps);
+    assert(abs((ux + uy + uz) - num_diff(f<double>, eps, x0, y0, z0)) < eqeps);
     clear(x, y, z);
 
     var v = g(x, y, z);
     v.propagate();
     auto vx = x.diff(), vy = y.diff(), vz = z.diff();
     cout << format("v = {:.5}, vx = {:.5}, vy = {:.5}, vz = {:.5}\n", v, vx, vy, vz);
-    assert(abs((vx + vy + vz) - numdiff(g<double>, eps, x0, y0, z0)) < eqeps);
+    assert(abs((vx + vy + vz) - num_diff(g<double>, eps, x0, y0, z0)) < eqeps);
     x.clear(), y.clear(), z.clear();
 
-    var nan_number = std::nan("");
-    var a = 1, b = 1, c = 2;
-    assert(a == b);
-    assert(nan_number != a);
-    assert(a < c);
-    assert(a <= c);
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,144 @@
+#include "src/optim.hpp"
+
+#include <random>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "lib/doctest.h"
+#include "src/variable.hpp"
+
+#include <cmath>
+
+const double eps = 1e-7;
+
+bool almost_equal(double a, double b) {
+    std::stringstream ss_a, ss_b;
+    ss_a << std::setprecision(6) << a;
+    ss_b << std::setprecision(6) << b;
+    return (ss_a.str() == ss_b.str()) || (std::abs(a - b) < 1e-6);
+}
+
+auto num_diff(auto f, auto eps, auto... args) {
+    return (f((args + eps)...) - f((args - eps)...)) / eps / 2;
+}
+
+std::tuple<double, double> get_diff(auto f, auto... args) {
+    auto var_args = std::make_tuple(var(args)...);
+    auto u = std::apply(f, var_args);
+    u.propagate();
+    double expected =
+        std::apply([](const auto&... args) { return (args.diff() + ...); }, var_args);
+    double result = num_diff(f, eps, args...);
+    return {expected, result};
+}
+
+bool check_diff(auto... args) {
+    auto [expected, result] = get_diff(args...);
+    return almost_equal(expected, result);
+}
+
+void check_impl(auto f, const std::string& name, auto... args) {
+    auto [expected, result] = get_diff(f, args...);
+    std::string args_str = ((std::to_string(args) + ", ") + ...);
+    args_str.pop_back(), args_str.pop_back();
+    CHECK_MESSAGE(almost_equal(expected, result), "in `", name, "`: autodiff ", expected,
+                  " real ", result, " when args = [", args_str, "]");
+}
+
+#define check_func(func, ...) check_impl(func, #func, __VA_ARGS__)
+
+TEST_CASE("div") {
+    auto div = [](auto x, auto y) { return x / y; };
+    check_func(div, -4, 3);
+}
+
+TEST_CASE("complex arithmetic operations") {
+    auto func = [](auto x, auto y, auto z) { return x * y + x / z - y * z; };
+    check_func(func, 2, 3, 4);
+    check_func(func, 2, 5, 3);
+    check_func(func, 100, 200, 500);
+    check_func(func, -15, -20, -15);
+}
+
+TEST_CASE("log/exp/sin/cos/tan/abs") {
+    auto func = [](auto x, auto y, auto z) {
+        return log(abs(x * z + 1)) + exp(x) * sin(y) - x * cos(y) + tan(z) + sin(x * y) -
+               exp(z) / (cos(x) + 1);
+    };
+    check_func(func, 2, 5, 3);
+    check_func(func, 4, 10, 2);
+    check_func(func, 2, -5, 3);
+    check_func(func, -4, 10, 2);
+}
+
+TEST_CASE("asin/acos/atan") {
+    auto func = [](auto x, auto y, auto z) {
+        return asin(x) + acos(y) - atan(z) + asin(z) * acos(x);
+    };
+    check_func(func, 0.5, 0.3, 0.7);
+    check_func(func, 0.1, 0.2, 0.3);
+    check_func(func, -0.1, -0.2, -0.3);
+}
+
+TEST_CASE("sinh/cosh/tanh") {
+    auto func = [](auto x, auto y, auto z) {
+        return sinh(x) + cosh(y) + tanh(z) * sinh(x);
+    };
+    check_func(func, 1, 10, -20);
+    check_func(func, -5, 14, -2);
+    check_func(func, 10, 1, 10);
+}
+
+TEST_CASE("sqrt/power") {
+    auto func = [](auto x, auto y, auto z) { return sqrt(x) + pow(y, 2) + pow(z, 3); };
+    check_func(func, 4, 2, 3);
+    check_func(func, 9, 3, 2);
+    check_func(func, 16, 4, 1);
+    check_func(func, 25, 5, 0);
+}
+
+TEST_CASE("compare") {
+    var nan_number = std::nan("");
+    var a = 1, b = 1, c = 2;
+    CHECK(a == b);
+    CHECK(nan_number != a);
+    CHECK(a < c);
+    CHECK(a <= c);
+}
+
+TEST_CASE("fitting") {
+    const int N = 100;
+    std::vector<double> xs, ys;
+    std::default_random_engine eng(42);
+    std::normal_distribution<double> noise(0.0, 1.0);
+
+    double k0 = 10.0, b0 = -5;
+    for (int i = 0; i < N; i++) {
+        double x = -10.0 + 20.0 * i / (N - 1);
+        double y = 10.0 * x + b0 + noise(eng);
+        xs.push_back(x);
+        ys.push_back(y);
+    }
+
+    var k = 0, b = 0, loss = 0;
+    // optim::GradientDescent optimizer({&k, &b}, 1e-3);
+    optim::Adam optimizer{{&k, &b}, 1};
+    const int iterations = 50;
+
+    for (int iter = 0; iter < iterations; iter++) {
+        var total_loss = 0.0;
+
+        for (int i = 0; i < N; i++) {
+            var diff = (k * xs[i] + b) - ys[i];
+            total_loss = total_loss + diff * diff;
+        }
+        total_loss = total_loss / N;
+        loss = total_loss;
+
+        total_loss.propagate();
+        optimizer.step();
+    }
+
+    INFO(std::format("fit result: (k0, b0) = ({}, {}), (k, b) = ({}, {}), loss = {}", k0,
+                     b0, k, b, loss));
+    CHECK(std::abs((k - k0).raw()) < 1.0);
+    CHECK(std::abs((b - b0).raw()) < 1.0);
+}
